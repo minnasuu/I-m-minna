fe3e21a2dc8ac811b43ede23516568abc494c8a9
minnasu <minnasu@tencent.com>
2025-12-12 03:02:07 +0800

    feat: add article editor, optimize docker deployment config, and fix types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
-/// <reference types="vite/client" />
+// declare module 'markdown-it';
+declare module 'markdown-it' {
+  const MarkdownIt: any;
+  export default MarkdownIt;
+}
+declare module 'react-markdown-editor-lite';
